876f3ef60649821dd67e8e04bf89deaec82d18fd
Sergio Vargas Poveda <sergiovarpov@gmail.com>
2024-08-01 20:00:08 -0500

    sql

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from db import db
 from controllers.ingredientes_controller import IngredientesController
+import pymysql
 
 import os
 
+
+pymysql.install_as_MySQLdb()
 
 load_dotenv()
 
@@ -15,7 +18,7 @@ secret_key =  os.urandom(24)
 
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{os.getenv("USER_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("URL_BD")}/{os.getenv("TABLE_BD")}'
-app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://sql10723330:CD9JplNqjh@sql10.freemysqlhosting.net/sql10723330'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://sql10723330:CD9JplNqjh@sql10.freemysqlhosting.net/sql10723330'
 app.config["SECRET_KEY"] = secret_key
 db.init_app(app)
 api = Api(app)
