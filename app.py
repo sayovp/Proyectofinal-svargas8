f635ef7933745c7461538f244fb48955d4bb4569
Sergio Vargas Poveda <sergiovarpov@gmail.com>
2024-08-02 19:20:27 -0500

    ajuste

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ secret_key =  os.urandom(24)
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql+pymysql://{os.getenv("USER_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("URL_BD")}/{os.getenv("TABLE_BD")}'
-#app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://sql10723330:CD9JplNqjh@sql10.freemysqlhosting.net/sql10723330'
+#app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql+pymysql://{os.getenv("USER_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("URL_BD")}/{os.getenv("TABLE_BD")}'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://sql10723330:CD9JplNqjh@sql10.freemysqlhosting.net/sql10723330'
 app.config["SECRET_KEY"] = secret_key
 db.init_app(app)
 api = Api(app)
